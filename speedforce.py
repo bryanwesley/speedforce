b28616fc8a5ec2a1e9df5bb908d62c7af50927ea
bryanwesley <bryansviana@hotmail.com>
2026-05-25 15:38:31 -0300

    add neg speedforce

--- speedforce.py
+++ speedforce.py
@@ -1,13 +1,18 @@
 # aqui temos um codigo representando a força de aceleração de uma forma orientada a objetos.
 class speedforce:
 # abaixo temos uma função para reconhecer cada parâmetro dela mesma.
-    def __init__(self, nome, eletricidade, poderes, usuario):
+    def __init__(self, nome, eletricidade, cor_do_raio, poderes, usuario):
        self.nome = nome
        self.funcionarios = eletricidade
        self.produtos = poderes
        self.usuario = usuario
+class negative_speedforce(speedforce):
+    def __init__(self, nome, eletricidade, cor_do_raio, poderes, usuario):
+        super().__init__(nome, eletricidade, cor_do_raio, poderes, usuario)
+        self._negatividade = []
+
 # aqui atribuio que força_original é o objeto speedforce que tem seus parâmetros personalizados.
-força_original = speedforce('F.A_original', "laranja", "velocidade","bryanwesley" )
+força_original = speedforce('F.A_original', "5000 volts", "laranja", "velocidade","Flash" )
 # temos abaixo uma serie de 'prints' para cada parâmetro.
 print(força_original)
 print(força_original.nome)
